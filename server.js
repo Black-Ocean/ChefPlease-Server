9b6235fdbb9deca91c01236f7102a8fc6f13d8de
Anton <anton.arboleda2gmail.com>
2017-01-03 12:16:18 -0800

    Setup up V1 file structure

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+const mainRoutes = require('./routes/')(app);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
