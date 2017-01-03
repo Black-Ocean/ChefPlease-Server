4fcc18643fc13d0fe90987e2c5310439d1c5d289
Anton <anton.arboleda2gmail.com>
2017-01-02 20:52:47 -0800

    Install bookshelf

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 
-
-
-
-
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(3000, function () {
