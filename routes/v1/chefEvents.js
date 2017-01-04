f77d5e9f3034a9aaecddaca516c33894b9366222
Anton <anton.arboleda2gmail.com>
2017-01-04 11:31:28 -0800

    Stage commits before rebase

---
+++ routes/v1/chefEvents.js
@@ -0,0 +1,20 @@
+module.exports = function(app) {
+  //get all chefs for a specific event
+  app.get('/chefEvent/:id', function () {
+    
+  }); 
+
+  //add supplied chef to event
+  app.post('/chefEvent/:id', function () {
+
+  });
+
+  //delete all chefs for a specific event
+  app.delete('/chefEvent/:id', function () {
+
+  });
+  //delete a specific chef for a specific event
+  app.delete('/chefEvent/:id/:chefId', function () {
+
+  });
+}
---
+++ routes/v1/userEvents.js
@@ -0,0 +1,22 @@
+module.exports = function(app) {
+  //Get all users for a specific event
+  app.get('/userEvent/:id', function () {
+
+  });
+  
+  //Add supplied user(s) to event
+  app.post('/userEvent/:id', function () {
+    
+  });
+
+  //Delete all users for a specific event
+  app.delete('/userEvent/:id', function () {
+
+  });
+
+  //Delete a specific chef for a specific event
+  app.delete('/userEvent/:id/:userId', function () {
+
+  });
+
+}
