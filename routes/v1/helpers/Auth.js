01399aa8df92d885d909e645ddf28ffc37b3bca1
Anton <anton.arboleda2gmail.com>
2017-01-05 00:13:06 -0800

    Complete userSignup route

--- routes/v1/helpers/Auth.js
+++ routes/v1/helpers/Auth.js
@@ -1,9 +1,15 @@
 //Utility functions for Authentication
+var Promise = require('bluebird');
+var bcrypt = require('bcrypt-nodejs');
 
 var isLoggedIn = function (req) {
   // if the session is true, log them in or log them out otherwise return false  
   return req.session ? !! req.session.user : false;
 };
+
+const getHash = (hash) => {
+  return hash;
+}
 
 exports.checkUser = function (req, res, next) {
   if (!isLoggedIn(req)) {
@@ -14,6 +20,7 @@ exports.checkUser = function (req, res, next) {
 };
 
 exports.createSession = function (req, res, newUser) {
+  // should also generate a web token to send back to the client to persist being logged in
   return req.session.regenerate(function () {
     //check what the user will be
     req.session.user = newUser;
@@ -21,9 +28,19 @@ exports.createSession = function (req, res, newUser) {
   });
 };
 
-exports.comparePassword = function (attemptedPassword) {
+exports.comparePassword = function (attemptedPassword, callback) {
   //FIX ME
   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
     callback(isMatch);
   });
 };
+
+exports.hashPassword = function(password) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(password, null, null).bind(this)
+    .then(function(hash) {
+      console.log(hash, 'hashed PW')
+      getHash(hash);
+      return hash
+    });
+}
