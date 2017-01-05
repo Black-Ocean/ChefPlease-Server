a30184b73d194841f24ad93e011c8b8b1b888507
adamjung <ajung91@hotmail.com>
2017-01-04 19:31:21 -0800

    Implement naive V1.0 routes for Anton's testing purposes

--- routes/v1/userChefRoutes.js
+++ routes/v1/userChefRoutes.js
@@ -1,6 +1,45 @@
+var url = require('url');
 const connection = require('../../db/index.js');
 
 module.exports = function(app) {
+  app.get('/users', function(req, res, next) {
+    let qString = 'SELECT * FROM users';
+    connection.query(qString, function(err, results) {
+      // TODO: filter results down and send to client
+      res.end(JSON.stringify({ data: results }));
+    });
+  });
+
+  app.post('/users', function(req, res, next) {
+    let user = req.body;
+    let qString = 'INSERT INTO users (name, bio, image) VALUES (?, ?, ?)';
+    connection.query(qString, [user.name, user.bio, user.image],
+      function(err, results) {
+        res.end(JSON.stringify({ data: results.insertId }));
+      }
+    );
+  });
+
+  app.put('/users/:id', function(req, res, next) {
+    let user = req.body;
+    let userID = req.params.id;
+    let qString = 'UPDATE users SET name = ?, bio = ?, image = ? WHERE id = ?';
+    console.log('upput this poop', userID);
+    connection.query(qString, [user.name, user.bio, user.image, userID], 
+      function(err, results) {
+        res.end(JSON.stringify({ data: results }));
+      }
+    )
+  });
+
+  app.delete('/users/:id', function(req, res, next) {
+    let userID = req.params.id;
+    let qString = 'DELETE FROM users WHERE id = ?';
+    connection.query(qString, [userID], function(err, results) {
+      res.end(JSON.stringify({ data: results }));
+    })
+  });
+
   app.get('/chefs', function(req, res, next) {
     let qString = 'SELECT * FROM chefs';
     connection.query(qString, function(err, results) {
@@ -18,66 +57,32 @@ module.exports = function(app) {
         // update users table
         let chefID = results.insertId;
         let qString = 'UPDATE users SET chefID = ? WHERE id = ?';
-        connection.query(qString, [chefID], function(err, results) {
-          res.end(chefId);
+        connection.query(qString, [chefID, chef.userID], function(err, results) {
+          // return id in chefs table for the new chef
+          res.end(JSON.stringify({ data: chefID }));
         });
       }
     );
   });
 
-  app.put('/chefs', function(req, res, next) {
+  app.put('/chefs/:id', function(req, res, next) {
     let chef = req.body;
+    let chefID = req.params.id;
     let qString = 'UPDATE chefs SET name = ?, bio = ?, image = ? WHERE id = ?';
-    connection.query(qString, [chef.name, chef.bio, chef.image, chef.userID],
+    connection.query(qString, [chef.name, chef.bio, chef.image, chefID],
       function(err, results) {
-        res.end(results);
+        res.end(JSON.stringify({ data: results }));
       }
     );
   });
 
-  app.delete('/chefs', function(req, res, next) {
+  app.delete('/chefs/:id', function(req, res, next) {
     // reset chefId in user's entry table
     let chef = req.body;
-    let chefID = req.query.id;
-    
+    let chefID = req.params.id;
     let qString = 'DELETE FROM chefs WHERE id = ?';
     connection.query(qString, [chefID], function(err, results) {
-      res.end(results);
+      res.end(JSON.stringify({ data: results }));
     });
-  });
-
-  app.get('/users', function(req, res, next) {
-    let qString = 'SELECT * FROM users';
-    connection.query(qString, function(err, results) {
-      res.end(results);
-    });
-  });
-
-  app.post('/users', function(req, res, next) {
-    let user = req.body;
-    let qString = 'INSERT INTO users (name, bio, image) VALUES (?, ?, ?)';
-    connection.query(qString, [user.name, user.bio, user.image],
-      function(err, results) {
-        res.end(results.insertId);
-      }
-    );
-  });
-
-  app.put('/users', function(req, res, next) {
-    let user = req.body;
-    let qString = 'UPDATE users SET name = ?, bio = ?, image = ? WHERE id = ?';
-    connection.query(qString, [user.name, user.bio, user.image], 
-      function(err, results) {
-        res.end(results);
-      }
-    )
-  });
-
-  app.delete('/users', function(req, res, next) {
-    let userID = req.query.id;
-    let qString = 'DELETE FROM users WHERE id = ?';
-    connection.query(qString, [userID], function(err, results) {
-      res.end(results);
-    })
   });
 }
