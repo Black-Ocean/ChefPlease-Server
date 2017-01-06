3a963aeaa4d90cfa5d3232f1f85d0106189acba8
Anton Arboleda <anton.arboleda@gmail.com>
2017-01-05 22:15:19 -0800

    Implement user signup, login and all middleware function

--- routes/v1/authRoutes.js
+++ routes/v1/authRoutes.js
@@ -2,9 +2,15 @@ const util = require('./helpers/Auth');
 
 module.exports = function (app) {
 
-  app.post('/login', util.login)
+  app.post('/login', util.login);
 
-  app.post('/signup', util.signUp)
+  app.post('/signup', util.signUp);
   
-}
+  app.get('/test', util.isLoggedIn, function (req, res) {
+    res.status(200).send('MIDDLE WARE WORKING');
+  });
+
+  app.get('/logout', util.logOut);
+
+};
 
