1eb0faafe6b1d7d87e074e1b0c369c86002b1966
adamjung <ajung91@hotmail.com>
2017-01-06 20:27:18 -0800

    Implement V1 routes for user, chef, events and dishes

---
+++ routes/v1/dishRoutes.js
@@ -0,0 +1,65 @@
+var url = require('url');
+const connection = require('../../db/index.js');
+
+module.exports = function(app) {
+  app.route('/dishes/chefs/:id')
+    .get(function(req, res, next) {
+      let chefId = req.params.id;
+      let qString = 'SELECT * FROM dishes WHERE id_chefID = ';
+
+      connection.query(qString, [chefID], function(err, results) {
+        res.send(JSON.stringify({ data: results }));
+      });
+    })
+    .post(function(req, res, next) {
+      let dish = req.body;
+      let chefId = req.params.id;
+      let qString = 'INSERT INTO dishes \
+                      (name, text, image, price, id_chefID) \
+                    VALUES (?, ?, ?, ?, ?)';
+
+      connection.query(qString, 
+        [dish.name, dish.text, dish.image, parseInt(dish.price), chefId],
+        function(err, results) {
+          if (err) {
+            res.sendStatus(500);
+          }
+          res.send(JSON.stringify({ data: results.insertId }));
+        }
+      );
+    });
+
+  app.route('/dishes/:dishId')
+    .put(function(req, res, next) {
+      let dish = req.body;
+      // let chefId = req.params.id;
+      let dishId = req.params.dishId;
+      let qString = 'UPDATE dishes SET name = ?, text = ?, image = ?, price = ?\
+                      WHERE id = ?';
+
+      connection.query(qString, 
+        [dish.name, dish.text, dish.image, parseInt(dish.price), dishId],
+        function(err, results) {
+          if (err) {
+            res.sendStatus(500);
+          }
+          res.sendStatus(200);
+        }
+      );
+      res.send(JSON.stringify({ data: insertId }));
+    })
+    .delete(function(req, res, next) {
+      let dish = req.body;
+      let dishId = req.params.dishId;
+      let qString = 'DELETE FROM dishes WHERE id = ?';
+      connection.query(qString,
+        [dishId],
+        function(err, results) {
+          if(err) {
+            res.sendStatus(500);
+          }
+          res.sendStatus(202);
+        }
+      );
+    });
+}
