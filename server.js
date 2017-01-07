9565adb54113d66a7d679099dc71d6bec8e237d6
Anton Arboleda <anton.arboleda@gmail.com>
2017-01-06 20:36:00 -0800

    Commit before rebase

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser');
 
 const util = require('./routes/v1/helpers/Auth');
 
+
+app.set('port', (process.env.PORT || 3000));
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/users', function (req, res, next) {
@@ -19,6 +22,6 @@ app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 const mainRoutes = require('./routes/')(app);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port', app.get('port'));
 });
