6df03951ab2ea16e9dd9ef955dd32be578903c4a
Anton <anton.arboleda2gmail.com>
2017-01-07 15:09:32 -0800

    Fix AuthToken

--- routes/v1/helpers/Auth.js
+++ routes/v1/helpers/Auth.js
@@ -8,7 +8,7 @@ const config = require('./config');
 
 // Middleware to protect view in the app
 exports.isLoggedIn = function (req, res, next) {
-  let AuthToken = req.headers.authtoken;
+  let AuthToken = req.headers.AuthToken;
   let query = 'Select * FROM tokens WHERE tokens.token=?'
   connection.query(query, [AuthToken], function (err, results) {
     // if the user in the database is found, 
