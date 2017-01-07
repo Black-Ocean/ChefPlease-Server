c2656db9e8591b8d7df25ab8436cf867ab40c5f7
adamjung <ajung91@hotmail.com>
2017-01-07 15:08:12 -0800

    Add get routes for locations, cuisines, and restrictions

---
+++ routes/v1/sideRoutes.js
@@ -0,0 +1,34 @@
+var url = require('url');
+const connection = require('../../db/index.js');
+
+module.exports = function(app) {
+  app.get('/locations', function(req, res, next) {
+    let qString = 'SELECT * FROM locations';
+    connection.query(qString, function(err, results) {
+      if (err) {
+        res.sendStatus(500);
+      }
+      res.send(JSON.stringify({ data: results }));
+    });
+  });
+
+  app.get('/cuisines', function(req, res, next) {
+    let qString = 'SELECT * FROM cuisines';
+    connection.query(qString, function(err, results) {
+      if (err) {
+        res.sendStatus(500);
+      }
+      res.send(JSON.stringify({ data: results }));
+    });
+  });
+
+  app.get('/restrictions', function(req, res, next) {
+    let qString = 'SELECT * FROM restrictions';
+    connection.query(qString, function(err, results) {
+      if (err) {
+        res.sendStatus(500);
+      }
+      res.send(JSON.stringify({ data: results }));
+    });
+  });
+}
